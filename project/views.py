aaadaaaa50aa476cf4e05a39aac5090f2a414e5f
Noam <noamoss@gmail.com>
2016-02-19 12:47:19 +0200

    add user id presentation on main view

--- project/views.py
+++ project/views.py
@@ -40,6 +40,7 @@ def login_required(test):
 @app.route('/logout/')
 def logout():
     session.pop('logged_in', None)
+    session.pop('user_id', None)
     flash('Goodbye!')
     return redirect(url_for('login'))
 
@@ -52,7 +53,8 @@ def login():
         if form.validate_on_submit():
             user = User.query.filter_by(name = request.form['name']).first()
             if user is not None and user.password == request.form['password']:
-                    session['logged_in']= True
+                    session['logged_in'] = True
+                    session['user_id'] = user.id
                     flash('Welcome!')
                     return redirect(url_for('tasks'))
             else:
@@ -88,7 +90,7 @@ def new_task():
                 form.priority.data,
                 '1',
                 datetime.datetime.utcnow().date(),
-                '1'
+                session['user_id']
             )
             db.session.add(new_task)
             db.session.commit()
